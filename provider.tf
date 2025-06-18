8307ad5b6a24fa6e4076245aeeecd180fb9ae166
iam-vihaan <asathyavihaan@gmail.com>
2025-06-18 15:09:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,16 @@
+# Configure the AWS Provider
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
   }
-  required_version = ">= 1.6.0"
 }
 
 provider "aws" {
